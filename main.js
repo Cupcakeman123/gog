1c61551d6c6e158b4912e42a6bab12e6a556dfab
Cupcakeman123 <75188459+Cupcakeman123@users.noreply.github.com>
2021-08-20 15:23:31 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,45 +1,37 @@
-var noseX = 0;
-var noseY = 0;
-
-function preload()
-{
-    clown_nose = loadImage("https://i.postimg.cc/kgNnZcrB/bob.jpg")
+function preload() {
+  clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
 }
 
-function setup()
-{
-    canvas = createCanvas(300, 300)
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.size(300, 300);
-    video.hide();
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
 
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on("pose", gotPoses);
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
-function modelLoaded()
-{
-    console.log("PoseNet Is Initalized");
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
 }
 
 function gotPoses(results)
 {
-    if(results.length > 0)
-    {
-        console.log(results);
-        console.log("nose x = " + result[0].poseNet.nose.x);
-        console.log("nose y = " + result[0].poseNet.nose.y);
-    }
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x-15;
+    noseY = results[0].pose.nose.y-15;
+  }
 }
 
-function draw()
-{
-    image(video, 0, 0, 300, 300);
-    image(clown_nose, noseX, noseY)
+function draw() {
+  image(video, 0, 0, 300, 300);
+  image(clown_nose, noseX, noseY, 30, 30);
 }
 
-function take_snapshot()
-{
-    save('myFliterImage.png')
+function take_snapshot(){    
+  save('myFilterImage.png');
 }
